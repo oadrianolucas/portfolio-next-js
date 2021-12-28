3d94a5c13e114f42890317cd3c1b81cbd99c6d43
unknown <adrianovitello@gmail.com>
2021-12-28 17:34:31 -0300

    -

--- pages/post.js
+++ pages/post.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="">
       <HeadTemplate />
-      <main className="flex min-h-screen text-gray-700 dark:text-gray-100  dark:bg-slate-900 ">
+      <main className="relative min-h-screen md:flex text-gray-700 dark:text-gray-100 dark:bg-slate-900 ">
         <Sidebar />
         <div className="flex-1">
           <Header />
